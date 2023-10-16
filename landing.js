e4a1e7ee06cf204c12a69893936110207f7ff2ef
Eric Miller <58384595+Attkless@users.noreply.github.com>
2023-10-15 20:00:30 -0500

    f

--- landing.js
+++ landing.js
@@ -1,12 +1,2 @@
-function getRandomRGB() {
-    const r = Math.floor(Math.random() * 256);
-    const g = Math.floor(Math.random() * 256);
-    const b = Math.floor(Math.random() * 256);
-    return `rgb(${r},${g},${b})`;
-  }
-  
-  function changeColor() {
-    const rgbBox = document.querySelector('.rgb-box');
-    rgbBox.style.backgroundColor = getRandomRGB();
-  }
+
   
